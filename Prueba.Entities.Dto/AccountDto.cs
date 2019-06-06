0036901b15dd0428797440273dad9a16f68a1152
duvan suarez olejua <duvansuo@hotmail.com>
2019-06-05 22:05:22 -0500

    nn

--- Prueba.Entities.Dto/AccountDto.cs
+++ Prueba.Entities.Dto/AccountDto.cs
@@ -11,6 +11,7 @@ namespace Prueba.Entities.Dto
         public long Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+
         public string Password { get; set; }
         public int IdRol { get; set; }
         public bool Status { get; set; }
